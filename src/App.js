4dd4861a5a8421a4c3f11bcc8689ae128fe419c7
Henrik <henkeh83@gmail.com>
2020-05-04 19:06:44 +0200

    Added PWA functionality

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import './App.css';
 import Board from './components/Board.js'
 
 function App() {
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  console.log('nu vart det triggat :)');
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  // showInstallPromotion();
+});
+
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Board />
+      <div className="App-logo">
+        <img src={logo} alt="logo" />
+        <button onClick={() => deferredPrompt}>Download app :)</button>
+      </div>
+      <Board />
     </div>
   );
 }
