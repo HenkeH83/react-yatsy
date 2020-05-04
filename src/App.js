57bafa0c06bc327373b4a831adcabf04644b845d
Henrik <henkeh83@gmail.com>
2020-05-04 19:28:38 +0200

    Added button for triggering PWA download

--- src/App.js
+++ src/App.js
@@ -7,20 +7,36 @@ function App() {
   let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('nu vart det triggat :)');
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
-  console.log('nu vart det triggat :)');
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
   // showInstallPromotion();
 });
 
+const runDownLoad (e) => {
+  console.log('Knappen är tryckt');
+  // Hide the app provided install promotion
+  // hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  })
+});
+
   return (
     <div className="App">
       <div className="App-logo">
         <img src={logo} alt="logo" />
-        <button onClick={() => deferredPrompt}>Download app :)</button>
+        <button onClick={() => runDownLoad()}>Download app :)</button>
       </div>
       <Board />
     </div>
